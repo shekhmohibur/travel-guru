a11f38d925d8e16803195ee4c08462d3e0da924d
Shekh Mohibur Rahman <mohibur.rahman2003@gmail.com>
2025-12-03 22:26:40 +0600

    project layout and files are created

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,9 +1,35 @@
 import { createBrowserRouter } from "react-router";
+import HomeLayout from "../layouts/HomeLayout";
+import Home from "../pages/Home";
 
 const Router = createBrowserRouter([
     {
         path:'/',
-        element:<h1>Travel Guru</h1>
+        element:<HomeLayout/>,
+        children:[
+            {
+                path:'',
+                element:<Home/>
+            },
+        ]
+    },
+    {
+        path:'/auth/',
+        element:<h1>AuthLayout</h1>,
+        children:[
+            {
+                path:'/auth/login',
+                element:<h2>login</h2>,
+            },
+            {
+                path:'/auth/register',
+                element:<h2>register</h2>,
+            },
+        ]
+    },
+    {
+        path:'/*',
+        element:<p>error 404</p>
     }
 ])
 export default Router;
---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,14 @@
+import NavBar from "../components/NavBar";
+
+const HomeLayout = () => {
+    return (
+        <div className="w-11/12 mx-auto">
+            <header>
+                <NavBar/>
+            </header>
+            <main></main>
+        </div>
+    );
+};
+
+export default HomeLayout;
