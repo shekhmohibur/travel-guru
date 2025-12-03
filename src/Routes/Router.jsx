7bd4b29ddd510e404c29af8959d65b4113810aef
Shekh Mohibur Rahman <mohibur.rahman2003@gmail.com>
2025-12-03 16:26:52 +0600

    Basic Structure building

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+
+const Router = createBrowserRouter([
+    {
+        path:'/',
+        element:<h1>Travel Guru</h1>
+    }
+])
+export default Router;
