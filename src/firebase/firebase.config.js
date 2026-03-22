c1d2699b687ac976e01520ae969d09d445ebd91b
Shekh Mohibur Rahman <119482264+shekhmohibur@users.noreply.github.com>
2026-03-22 16:45:10 +0600

    updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyBm2WZV3RRA_o0LJt2I0XXGlxDw86TVkdc",
-  authDomain: "travelguruv1.firebaseapp.com",
-  projectId: "travelguruv1",
-  storageBucket: "travelguruv1.firebasestorage.app",
-  messagingSenderId: "492790272560",
-  appId: "1:492790272560:web:285a79f8ec33a6eb68d3f5"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
